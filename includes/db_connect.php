ac8742956259fd4edd48a7277eb5160591ed115b
Ritesh Dhungel <139316061+ReD9541@users.noreply.github.com>
2024-11-09 00:13:42 +1100

    Update database connection details for production environment

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,8 +1,13 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db = "reviewmate";
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $db = "reviewmate";
+
+    $servername = '153.92.15.26';
+    $db = 'u791027335_movie_db';
+    $username = 'u791027335_admin';
+    $password = 'l7HGSK8|';
 
     $conn = mysqli_connect($servername, $username, $password, $db);
 
